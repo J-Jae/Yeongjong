2ce168a19c490295e90b6eb67f76ad489e32a15f
Jae <dotori0807@gmail.com>
2020-11-24 23:50:01 +0900

    add case

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
 import osmnx as ox
 import networkx as nx
 import osmnxUtils
-OUTPUT_PATH = './data'
+
+# get some bbox
+bbox = ox.utils_geo.bbox_from_point((45.518698, -122.679964), dist=300)
+north, south, east, west = bbox
+G = ox.graph_from_bbox(north, south, east, west, network_type='drive', clean_periphery=False)
+fig, ax = ox.plot_graph(G, node_color='r')
+
+# the node degree distribution for this graph has many false cul-de-sacs
+k = dict(G.degree())
+{n:list(k.values()).count(n) for n in range(max(k.values()) + 1)}
+G = ox.graph_from_bbox(north, south, east, west, network_type='drive')
+fig, ax = ox.plot_graph(G, node_color='r')
+
+district_name = 'simple'
+osmnxUtils.get_data(G, district_name)
 
 
+
+"""
 # ex_1 : SAN FRANCISCO
 district_name = 'San_Francisco'
 ox.config(all_oneway=True)
 G = ox.graph_from_bbox(37.79, 37.78, -122.41, -122.43, network_type='drive')
 
-osmnxUtils.get_data(G, district_name)
-osmnxUtils.plot(G)
+# osmnxUtils.get_data(G, district_name)
+G_proj = ox.project_graph(G)
+fig, ax = ox.plot_graph(G_proj) 
 
 # ex_2 : wurster_hall
 district_name = 'Wurster_Hall'
 wurster_hall = (37.870605, -122.254830)
-one_mile = 1609 #meters
-# one_mile = 500 #meters
+one_mile = 1609 #meters  alt : 500 meters
 G = ox.graph_from_point(wurster_hall, dist=one_mile, network_type='drive')
 
-osmnxUtils.get_data(G, district_name)
-osmnxUtils.plot(G)
+# osmnxUtils.get_data(G, district_name)
+fig, ax = ox.plot_graph(G, node_size=0)
+
+
+# ex_3 : get NY subway rail network
+district_name = 'NY subway rail'
+G = ox.graph_from_place('New York City, New York',
+                        retain_all=False, truncate_by_edge=True, simplify=True,
+                        custom_filter='["railway"~"subway"]')
+
+# osmnxUtils.get_data(G, district_name)
+fig, ax = ox.plot_graph(G, node_size=0, edge_color='w', edge_linewidth=0.2)
+
+"""
--- osmnxUtils.py
+++ osmnxUtils.py
@@ -7,9 +7,11 @@ OUTPUT_PATH = './data'
 
 def get_data(G, district_name):
     print(nx.info(G))
+    print(f'File Path = {OUTPUT_PATH}')
 
     # save as .osm
     path = f'{OUTPUT_PATH}/{district_name}.osm'
+    ox.config(all_oneway=True)
     if not os.path.exists(path):
         ox.save_graph_xml(G, filepath=path)
 
@@ -25,69 +27,10 @@ def get_data(G, district_name):
         A = nx.to_numpy_matrix(G)
         np.savetxt(path, A, delimiter=",")
 
-def plot(G, node_size=0):
-    G_proj = ox.project_graph(G)
-    fig, ax = ox.plot_graph(G_proj, node_size=node_size)  # ox.plot_graph(G)
-    
+    # save as SVG
+    path = f'{OUTPUT_PATH}/{district_name}_image.svg'
+    fig, ax = ox.plot_graph(G, show=False, save=True, close=True, filepath=path)
 
-# ox.plot_graph(ox.graph_from_place('Modena, Italy'))
-
-# ex_1 : SAN FRANCISCO
-
-# district_name = 'San_Francisco'
-
-# # get NetworkX Graph
-# ox.config(all_oneway=True)
-# G = ox.graph_from_bbox(37.79, 37.78, -122.41, -122.43, network_type='drive')
-
-# # print graph infomation
-# print(nx.info(G))
-
-# # plot graph
-# G_proj = ox.project_graph(G)
-# fig, ax = ox.plot_graph(G_proj)  # ox.plot_graph(G)
-
-# # save as .osm
-# path = f'{OUTPUT_PATH}/{district_name}.osm'
-# if not os.path.exists(path):
-#     ox.save_graph_xml(G, filepath=path)
-
-# # save as folium html
-# path = f'{OUTPUT_PATH}/{district_name}_folium.html'
-# if not os.path.exists(path):
-#     map_folium = ox.folium.plot_graph_folium(G)
-#     map_folium.save(path)
-
-# # save as adjacency matrix
-# path = f'{OUTPUT_PATH}/{district_name}_matrix.csv'
-# if not os.path.exists(path):
-#     A = nx.to_numpy_matrix(G)
-#     np.savetxt(path, A, delimiter=",")
-
-
-# # ex_1 : SAN FRANCISCO
-
-# district_name = 'San_Francisco'
-
-# # get NetworkX Graph
-# ox.config(all_oneway=True)
-# wurster_hall = (37.870605, -122.254830)
-# one_mile = 1609 #meters
-# G = ox.graph_from_point(wurster_hall, dist=one_mile, network_type='drive')
-# fig, ax = ox.plot_graph(G, node_size=0)
-
-# # print graph infomation
-# print(nx.info(G))
-
-# # plot graph
-# G_proj = ox.project_graph(G)
-# ox.plot_graph(G_proj)  # ox.plot_graph(G)
-
-# # save as .osm
-# ox.save_graph_xml(G, filepath=f'{OUTPUT_PATH}/{district_name}.osm')
-
-# # save as folium html
-# map_folium = ox.folium.plot_graph_folium(G)
-# map_folium.save(f'{OUTPUT_PATH}/{district_name}_folium.html')
-
-# # save as adjacency matrix
+    # save graph as a shapefile
+    path = f'{OUTPUT_PATH}/{district_name}_shape'
+    ox.save_graph_shapefile(G, filepath=path)
